6878dfa3a82dfef134fddb126861cea224d77c58
Danica Basalo <basalodan@gmail.com>
2024-10-13 19:43:09 +0800

    transaction ui

--- Laboratory Management System (Capstone Project)/TransactionDetails.Designer.cs
+++ Laboratory Management System (Capstone Project)/TransactionDetails.Designer.cs
@@ -78,22 +78,22 @@
             // lblBorrow
             // 
             this.lblBorrow.AutoSize = true;
-            this.lblBorrow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBorrow.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
             this.lblBorrow.Location = new System.Drawing.Point(562, 203);
             this.lblBorrow.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblBorrow.Name = "lblBorrow";
-            this.lblBorrow.Size = new System.Drawing.Size(192, 20);
+            this.lblBorrow.Size = new System.Drawing.Size(177, 19);
             this.lblBorrow.TabIndex = 2;
             this.lblBorrow.Text = "Borrowed Apparatuses";
             // 
             // lblReturn
             // 
             this.lblReturn.AutoSize = true;
-            this.lblReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReturn.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
             this.lblReturn.Location = new System.Drawing.Point(562, 223);
             this.lblReturn.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblReturn.Name = "lblReturn";
-            this.lblReturn.Size = new System.Drawing.Size(191, 20);
+            this.lblReturn.Size = new System.Drawing.Size(172, 19);
             this.lblReturn.TabIndex = 3;
             this.lblReturn.Text = "Returned Apparatuses";
             // 
@@ -127,44 +127,49 @@
             // lblViolation
             // 
             this.lblViolation.AutoSize = true;
-            this.lblViolation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblViolation.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
             this.lblViolation.Location = new System.Drawing.Point(580, 243);
             this.lblViolation.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblViolation.Name = "lblViolation";
-            this.lblViolation.Size = new System.Drawing.Size(151, 20);
+            this.lblViolation.Size = new System.Drawing.Size(136, 19);
             this.lblViolation.TabIndex = 41;
             this.lblViolation.Text = "Violation Records";
             // 
             // cmbViewOptions
             // 
-            this.cmbViewOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbViewOptions.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
             this.cmbViewOptions.FormattingEnabled = true;
-            this.cmbViewOptions.Location = new System.Drawing.Point(308, 173);
+            this.cmbViewOptions.Location = new System.Drawing.Point(283, 134);
             this.cmbViewOptions.Margin = new System.Windows.Forms.Padding(2);
             this.cmbViewOptions.Name = "cmbViewOptions";
-            this.cmbViewOptions.Size = new System.Drawing.Size(177, 28);
+            this.cmbViewOptions.Size = new System.Drawing.Size(177, 27);
             this.cmbViewOptions.TabIndex = 42;
             this.cmbViewOptions.SelectedIndexChanged += new System.EventHandler(this.cmbViewOptions_SelectedIndexChanged);
             // 
             // btnPrint
             // 
+            this.btnPrint.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnPrint.FlatAppearance.BorderSize = 0;
+            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrint.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
+            this.btnPrint.ForeColor = System.Drawing.Color.White;
             this.btnPrint.Location = new System.Drawing.Point(957, 150);
             this.btnPrint.Margin = new System.Windows.Forms.Padding(2);
             this.btnPrint.Name = "btnPrint";
             this.btnPrint.Size = new System.Drawing.Size(154, 29);
             this.btnPrint.TabIndex = 43;
             this.btnPrint.Text = "PRINT";
-            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.UseVisualStyleBackColor = false;
             this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
             // lblApparatus
             // 
             this.lblApparatus.AutoSize = true;
-            this.lblApparatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApparatus.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
             this.lblApparatus.Location = new System.Drawing.Point(562, 266);
             this.lblApparatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblApparatus.Name = "lblApparatus";
-            this.lblApparatus.Size = new System.Drawing.Size(234, 20);
+            this.lblApparatus.Size = new System.Drawing.Size(215, 19);
             this.lblApparatus.TabIndex = 44;
             this.lblApparatus.Text = "Laboratory Inventory Report";
             // 
@@ -183,11 +188,11 @@
             // lblStudents
             // 
             this.lblStudents.AutoSize = true;
-            this.lblStudents.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStudents.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
             this.lblStudents.Location = new System.Drawing.Point(562, 294);
             this.lblStudents.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblStudents.Name = "lblStudents";
-            this.lblStudents.Size = new System.Drawing.Size(266, 20);
+            this.lblStudents.Size = new System.Drawing.Size(241, 19);
             this.lblStudents.TabIndex = 46;
             this.lblStudents.Text = "Registered Laboratory Students";
             // 
@@ -207,23 +212,29 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(116, 176);
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(91, 137);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(188, 20);
+            this.label1.Size = new System.Drawing.Size(168, 19);
             this.label1.TabIndex = 49;
             this.label1.Text = "Check Record Details:";
             // 
             // btnExport
             // 
+            this.btnExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnExport.FlatAppearance.BorderSize = 0;
+            this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExport.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold);
+            this.btnExport.ForeColor = System.Drawing.Color.White;
             this.btnExport.Location = new System.Drawing.Point(957, 116);
             this.btnExport.Margin = new System.Windows.Forms.Padding(2);
             this.btnExport.Name = "btnExport";
             this.btnExport.Size = new System.Drawing.Size(154, 29);
             this.btnExport.TabIndex = 50;
             this.btnExport.Text = "EXPORT";
-            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.UseVisualStyleBackColor = false;
             this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
             // 
             // TransactionDetails
@@ -232,7 +243,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(1195, 894);
+            this.BackgroundImage = global::Laboratory_Management_System__Capstone_Project_.Properties.Resources.bg_popup_main;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1195, 884);
             this.Controls.Add(this.btnExport);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvStudents);
