5389cebfa8b9d53bf4454f43f9e1ae9926c48004
Raul Vincent Rodriguez <124639153+Lockheer@users.noreply.github.com>
2024-10-25 10:02:49 +0800

    - Fixed Startup

--- Laboratory Management System (Capstone Project)/Program.cs
+++ Laboratory Management System (Capstone Project)/Program.cs
@@ -22,8 +22,7 @@ namespace Laboratory_Management_System__Capstone_Project_
             SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashboard());
-            //Application.Run(new Startup());
+            Application.Run(new Startup());
             //Application.Run(new UpdateBorrowReturnTransaction());
 
 
